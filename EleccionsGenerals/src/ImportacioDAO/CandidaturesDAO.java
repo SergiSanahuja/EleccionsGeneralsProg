e86fa80ede2da33443272fba5cb44cc5b273c626
Sergi Sanahuja <80123937+SergiSanahuja@users.noreply.github.com>
2023-05-11 09:25:46 +0200

    Candidatures fet

--- EleccionsGenerals/src/ImportacioDAO/CandidaturesDAO.java
+++ EleccionsGenerals/src/ImportacioDAO/CandidaturesDAO.java
@@ -4,6 +4,7 @@ import Clases.Candidatures;
 import Clases.Main;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class CandidaturesDAO implements DAO<Candidatures> {
 
@@ -27,16 +28,42 @@ public class CandidaturesDAO implements DAO<Candidatures> {
 
     @Override
     public void read(Candidatures candidatures) {
+        try {
+            PreparedStatement ps = Main.con.prepareStatement("SELECT * FROM candidatures WHERE candidatura_id = ?");
+            ps.setInt(1, candidatures.getCandidaturaID());
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                System.out.println(rs.getInt("eleccio_id") + " | " + rs.getInt("codi_candidatura") + " | " + rs.getString("nom_curt") + "  | " + rs.getString("nom_llarg") + " | " + rs.getInt("codi_acumulacio_provincia") + " | " + rs.getInt("codi_acumulacio_ca") + " | " + rs.getInt("codi_acumulario_nacional"));
+            }
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 
     @Override
     public void update(Candidatures candidatures) {
-
+        try {
+            PreparedStatement ps = Main.con.prepareStatement("UPDATE candidatures SET nom_curt = ?, nom_llarg = ? WHERE candidatura_id = ?");
+            ps.setString(1, candidatures.getNom_curt());
+            ps.setString(2, candidatures.getNom_llarg());
+            ps.setInt(3, candidatures.getCandidaturaID());
+            int columnMod = ps.executeUpdate();
+            System.out.println("Columenes modificades: " + columnMod);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void delete(Candidatures candidatures) {
-
+        try{
+            PreparedStatement ps = Main.con.prepareStatement("DELETE FROM candidatures WHERE candidatura_id = ?");
+            ps.setInt(1, candidatures.getCandidaturaID());
+            int columnMod = ps.executeUpdate();
+            System.out.println("Columenes eliminades: " + columnMod);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
